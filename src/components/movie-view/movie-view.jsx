9bd77c5778ccf93325d38411ccea578116e0923e
ajackonski <113149629+ajackonski@users.noreply.github.com>
2024-08-09 17:28:20 -0500

    added hook and proptypes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,12 +1,35 @@
+import PropTypes from 'prop-types';
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
         <h1>{movie.title}</h1>
-        <img src={movie.image} alt={movie.title} />
+        <img src={movie.imagePath} alt={movie.title} />
         <p>{movie.description}</p>
-        <p>{movie.genre}</p>
-        <p>{movie.director}</p>
+        <p>{movie.genre.name}</p>
+        <p>{movie.director.name}</p>
         <button onClick={onBackClick}>Back</button>
       </div>
     );
-  };
+};
+
+// Adding PropTypes for the component
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string
+    }).isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string
+    }).isRequired,
+    description: PropTypes.string.isRequired,
+    actors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    featured: PropTypes.bool.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
+};
